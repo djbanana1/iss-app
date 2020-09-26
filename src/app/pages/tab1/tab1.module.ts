cdbbaa3802beae0c7782bb07587b37a8f9164922
Luca Derivaz <lucaderivaz@Lucas-MacBook-Pro.local>
2020-09-26 23:16:26 +0200

    remove - global : unused tabs

--- src/app/pages/tab1/tab1.module.ts
+++ src/app/pages/tab1/tab1.module.ts
@@ -1,20 +1,12 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
-import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { IonicModule } from '@ionic/angular'
+import { Tab1PageRoutingModule } from './tab1-routing.module'
+import { Tab1Page } from './tab1.page'
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    Tab1PageRoutingModule
-  ],
-  declarations: [Tab1Page]
+  imports: [IonicModule, CommonModule, FormsModule, Tab1PageRoutingModule],
+  declarations: [Tab1Page],
 })
 export class Tab1PageModule {}
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -12,14 +12,6 @@ const routes: Routes = [
         loadChildren: () => import('../pages/tab1/tab1.module').then((m) => m.Tab1PageModule),
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../pages/tab2/tab2.module').then((m) => m.Tab2PageModule),
-      },
-      {
-        path: 'tab3',
-        loadChildren: () => import('../pages/tab3/tab3.module').then((m) => m.Tab3PageModule),
-      },
-      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full',
