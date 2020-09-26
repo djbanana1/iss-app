6a6dcb85ae0d2c7eef8e44c13b8a6c8f1762779a
Luca Derivaz <lucaderivaz@Lucas-MacBook-Pro.local>
2020-09-27 00:11:52 +0200

    create - model

--- src/app/pages/model/model.module.ts
+++ src/app/pages/model/model.module.ts
@@ -1,15 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { ModelRoutingModule } from './model-routing.module';
-import { ModelComponent } from './model.component';
-
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { IonicModule } from '@ionic/angular'
+import { ModelRoutingModule } from './model-routing.module'
+import { ModelComponent } from './model.component'
 
 @NgModule({
   declarations: [ModelComponent],
-  imports: [
-    CommonModule,
-    ModelRoutingModule
-  ]
+  imports: [CommonModule, ModelRoutingModule, IonicModule],
 })
-export class ModelModule { }
+export class ModelModule {}
