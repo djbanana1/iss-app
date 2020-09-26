26d8987d677a0e06b12f12b79890cb04de7ec187
Luca Derivaz <lucaderivaz@Lucas-MacBook-Pro.local>
2020-09-27 00:58:08 +0200

    add - issModel

--- src/app/pages/model/model.component.ts
+++ src/app/pages/model/model.component.ts
@@ -1,12 +1,46 @@
-import { Component, OnInit } from '@angular/core'
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core'
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 @Component({
   selector: 'model',
   templateUrl: './model.component.html',
   styleUrls: ['./model.component.scss'],
 })
-export class ModelComponent implements OnInit {
-  constructor() {}
+export class ModelComponent implements AfterViewInit {
+  @ViewChild('rendererContainer') rendererContainer: ElementRef
 
-  ngOnInit(): void {}
+  renderer = new THREE.WebGLRenderer()
+  scene = null
+  camera = null
+  mesh = null
+
+  constructor() {
+    let loader = new GLTFLoader()
+
+    this.scene = new THREE.Scene()
+
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000)
+    this.camera.position.z = 1000
+
+    loader.load(
+      'assets/model/iss.glb',
+      function (gltf) {
+        console.log(gltf)
+        this.scene.add(gltf.scene)
+      },
+      undefined,
+      function (error) {
+        console.error(error)
+      },
+    )
+  }
+
+  ngAfterViewInit() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.rendererContainer.nativeElement.appendChild(this.renderer.domElement)
+    this.animate()
+  }
+
+  animate() {}
 }
