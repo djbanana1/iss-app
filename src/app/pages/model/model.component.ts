7a6379107aac7baae9ec3453cd579a35fbf87d9c
Luca Derivaz <lucaderivaz@Lucas-MacBook-Pro.local>
2020-09-27 02:37:22 +0200

    add - issModel : ui

--- src/app/pages/model/model.component.ts
+++ src/app/pages/model/model.component.ts
@@ -16,31 +16,42 @@ export class ModelComponent implements AfterViewInit {
   mesh = null
 
   constructor() {
-    let loader = new GLTFLoader()
+    var loader = new GLTFLoader()
 
     this.scene = new THREE.Scene()
+    this.scene.background = new THREE.Color(0xffffff)
 
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000)
     this.camera.position.z = 1000
 
+    const geometry = new THREE.BoxGeometry(200, 200, 200)
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    this.mesh = new THREE.Mesh(geometry, material)
+
     loader.load(
       'assets/model/iss.glb',
-      function (gltf) {
+      (gltf) => {
         console.log(gltf)
-        this.scene.add(gltf.scene)
+        let sceneModel = gltf.scene
+        this.scene.add(sceneModel)
       },
-      undefined,
-      function (error) {
+      (loading) => {},
+      (error) => {
         console.error(error)
       },
     )
   }
 
   ngAfterViewInit() {
-    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.setSize(window.innerWidth, window.innerHeight - 62)
     this.rendererContainer.nativeElement.appendChild(this.renderer.domElement)
     this.animate()
   }
 
-  animate() {}
+  animate() {
+    window.requestAnimationFrame(() => this.animate())
+    this.mesh.rotation.x += 0.01
+    this.mesh.rotation.y += 0.02
+    this.renderer.render(this.scene, this.camera)
+  }
 }
