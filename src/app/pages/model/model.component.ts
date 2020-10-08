e16ec16b1ddc02bf30581bec4516ae2be9cba57a
Luca Derivaz <lucaderivaz@Lucas-MacBook-Pro.local>
2020-10-08 15:34:23 +0200

    add - model : orbit controls

--- src/app/pages/model/model.component.ts
+++ src/app/pages/model/model.component.ts
@@ -1,7 +1,6 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core'
-import * as THREE from 'three'
-import 'three/examples/js/controls/OrbitControls'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { OrbitControls } from '@avatsaev/three-orbitcontrols-ts';
+import * as THREE from 'three';
 
 @Component({
   selector: 'model',
@@ -9,54 +8,61 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
   styleUrls: ['./model.component.scss'],
 })
 export class ModelComponent implements AfterViewInit {
-  @ViewChild('rendererContainer') rendererContainer: ElementRef
+  @ViewChild('rendererContainer') rendererContainer: ElementRef;
 
-  renderer = new THREE.WebGLRenderer({ antialias: true })
-  scene = null
-  camera = null
-  mesh = null
+  renderer = new THREE.WebGLRenderer({ alpha: true });
+  scene = null;
+  camera = null;
+  mesh = null;
+  controls = null;
 
   constructor() {
-    var loader = new GLTFLoader()
-
-    this.scene = new THREE.Scene()
-    this.scene.background = new THREE.Color(0xffffff)
-
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000)
-    this.camera.position.z = 200
-
-    let controls = new THREE.OrbitControls(this.camera, this.renderer.domElement)
-    controls.addEventListener('change', this.renderer)
-
-    // let hLight = new THREE.AmbientLight(0x404040, 100)
-    // this.scene.add(hLight)
-
-    let directionalLight = new THREE.DirectionalLight(0xffffff, 100)
-    directionalLight.position.set(0, 10, 0)
-    directionalLight.castShadow = true
-    this.scene.add(directionalLight)
-
-    loader.load(
-      'assets/model/iss.glb',
-      (gltf) => {
-        gltf.scene.rotateY((45 / 180) * Math.PI)
-        this.scene.add(gltf.scene)
-      },
-      (loading) => {},
-      (error) => {
-        console.error(error)
-      },
-    )
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    this.controls = new OrbitControls(this.camera);
   }
 
   ngAfterViewInit() {
-    this.renderer.setSize(window.innerWidth, window.innerHeight - 62)
-    this.rendererContainer.nativeElement.appendChild(this.renderer.domElement)
-    this.animate()
+    this.configCamera();
+    this.configRenderer();
+    this.configControls();
+
+    this.createMesh();
+
+    this.animate();
+  }
+
+  configCamera() {
+    this.camera.position.set(300, 300, 300);
+  }
+
+  configRenderer() {
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setClearColor(new THREE.Color("hsl(0, 0%, 10%)"));
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.domElement.style.display = "block";
+    this.renderer.domElement.style.margin = "auto";
+    this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
+  }
+
+  configControls() {
+    this.controls.autoRotate = true;
+    this.controls.enableZoom = false;
+    this.controls.enablePan  = false;
+    this.controls.update();
+  }
+
+  createMesh() {
+    const geometry = new THREE.BoxGeometry(200, 200, 200);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff7f50 });
+    this.mesh = new THREE.Mesh(geometry, material);
+
+    this.scene.add(this.mesh);
   }
 
   animate() {
-    window.requestAnimationFrame(() => this.animate())
-    this.renderer.render(this.scene, this.camera)
+    window.requestAnimationFrame(() => this.animate());
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
   }
 }
