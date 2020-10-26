89a4dcb706658b630309b7a4b6fef844f404ef9c
Luca Derivaz <lucaderivaz@Lucas-MacBook-Pro.local>
2020-10-26 22:19:29 +0100

    add - model : ambienteLight

--- src/app/pages/model/model.component.ts
+++ src/app/pages/model/model.component.ts
@@ -1,6 +1,7 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { OrbitControls } from '@avatsaev/three-orbitcontrols-ts';
-import * as THREE from 'three';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core'
+import { OrbitControls } from '@avatsaev/three-orbitcontrols-ts'
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 @Component({
   selector: 'model',
@@ -8,61 +9,73 @@ import * as THREE from 'three';
   styleUrls: ['./model.component.scss'],
 })
 export class ModelComponent implements AfterViewInit {
-  @ViewChild('rendererContainer') rendererContainer: ElementRef;
+  @ViewChild('rendererContainer') rendererContainer: ElementRef
 
-  renderer = new THREE.WebGLRenderer({ alpha: true });
-  scene = null;
-  camera = null;
-  mesh = null;
-  controls = null;
+  renderer = new THREE.WebGLRenderer({ alpha: true })
+  scene = null
+  camera = null
+  mesh = null
+  controls = null
+  loader = null
 
   constructor() {
-    this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-    this.controls = new OrbitControls(this.camera);
+    this.scene = new THREE.Scene()
+    this.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1000)
+    this.controls = new OrbitControls(this.camera)
+    this.loader = new GLTFLoader()
+
+    // add lights
+    var light = new THREE.AmbientLight(0xffffff)
+    this.scene.add(light)
   }
 
   ngAfterViewInit() {
-    this.configCamera();
-    this.configRenderer();
-    this.configControls();
+    this.configCamera()
+    this.configRenderer()
+    this.configControls()
 
-    this.createMesh();
+    this.createMesh()
 
-    this.animate();
+    this.animate()
   }
 
   configCamera() {
-    this.camera.position.set(300, 300, 300);
+    this.camera.position.set(300, 300, 300)
   }
 
   configRenderer() {
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setClearColor(new THREE.Color("hsl(0, 0%, 10%)"));
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.domElement.style.display = "block";
-    this.renderer.domElement.style.margin = "auto";
-    this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.renderer.setClearColor(new THREE.Color('hsl(0, 0%, 10%)'))
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.domElement.style.display = 'block'
+    this.renderer.domElement.style.margin = 'auto'
+    this.rendererContainer.nativeElement.appendChild(this.renderer.domElement)
   }
 
   configControls() {
-    this.controls.autoRotate = true;
-    this.controls.enableZoom = false;
-    this.controls.enablePan  = false;
-    this.controls.update();
+    this.controls.autoRotate = true
+    this.controls.enableZoom = true
+    this.controls.enablePan = false
+    this.controls.update()
   }
 
-  createMesh() {
-    const geometry = new THREE.BoxGeometry(200, 200, 200);
-    const material = new THREE.MeshBasicMaterial({ color: 0xff7f50 });
-    this.mesh = new THREE.Mesh(geometry, material);
-
-    this.scene.add(this.mesh);
+  async createMesh() {
+    await this.loader.load(
+      'assets/model/iss.glb',
+      (gltf) => {
+        console.log(gltf)
+        let sceneModel = gltf.scene
+        this.scene.add(sceneModel)
+      },
+      (loading) => {},
+      (error) => {
+        console.error(error)
+      },
+    )
   }
 
   animate() {
-    window.requestAnimationFrame(() => this.animate());
-    this.controls.update();
-    this.renderer.render(this.scene, this.camera);
+    window.requestAnimationFrame(() => this.animate())
+    this.renderer.render(this.scene, this.camera)
   }
 }
