abd3c8329e3e9487df320889e9bc51bd4775e905
Luca Derivaz <lucaderivaz@Lucas-MacBook-Pro.local>
2020-12-04 08:31:46 +0100

    add - model : loading

--- src/app/pages/model/model.component.ts
+++ src/app/pages/model/model.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core'
 import { OrbitControls } from '@avatsaev/three-orbitcontrols-ts'
 import * as THREE from 'three'
+import { Interaction } from 'three.interaction'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 @Component({
@@ -11,21 +12,27 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 export class ModelComponent implements AfterViewInit {
   @ViewChild('rendererContainer') rendererContainer: ElementRef
 
+  loading = false
+
   renderer = new THREE.WebGLRenderer({ alpha: true })
   scene = null
   camera = null
   mesh = null
   controls = null
   loader = null
+  interaction = null
+  sceneModel
 
   constructor() {
+    this.loading = true
     this.scene = new THREE.Scene()
     this.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1000)
     this.controls = new OrbitControls(this.camera)
     this.loader = new GLTFLoader()
+    this.interaction = new Interaction(this.renderer, this.scene, this.camera)
 
     // add lights
-    var light = new THREE.AmbientLight(0xffffff)
+    var light = new THREE.AmbientLight(0xffffff, 1.5)
     this.scene.add(light)
   }
 
@@ -59,13 +66,14 @@ export class ModelComponent implements AfterViewInit {
     this.controls.update()
   }
 
-  async createMesh() {
-    await this.loader.load(
+  createMesh() {
+    this.loader.load(
       'assets/model/iss.glb',
       (gltf) => {
         console.log(gltf)
-        let sceneModel = gltf.scene
-        this.scene.add(sceneModel)
+        this.sceneModel = gltf.scene
+        this.scene.add(this.sceneModel)
+        this.loading = false
       },
       (loading) => {},
       (error) => {
