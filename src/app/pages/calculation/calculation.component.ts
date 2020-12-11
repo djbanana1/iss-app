3154ed3151dd19c67ff2d01938bf56af249c6aea
Luca Derivaz <lucaderivaz@Lucas-MacBook-Pro.local>
2020-12-11 08:25:22 +0100

    enhance - page/calculation

--- src/app/pages/calculation/calculation.component.ts
+++ src/app/pages/calculation/calculation.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Component } from '@angular/core'
+import { FormBuilder } from '@angular/forms'
 import { Geolocation } from '@ionic-native/geolocation/ngx'
 
 @Component({
@@ -6,59 +8,84 @@ import { Geolocation } from '@ionic-native/geolocation/ngx'
   templateUrl: './calculation.component.html',
   styleUrls: ['./calculation.component.scss'],
 })
-export class CalculationComponent implements OnInit {
-  public iss_latitude;
-  public iss_longitude;
-  public gps_latitude;
-  public gps_longitude;
-  public current_place;
-  public current_zip;
+export class CalculationComponent {
+  iss_latitude
+  iss_longitude
+  gps_latitude
+  gps_longitude
+  current_place
+  current_zip
 
-  constructor(private geolocation: Geolocation) {
+  swiper = false
+  itemForm
 
-    this.geolocation.getCurrentPosition().then((resp) => {
-      this.gps_latitude = resp.coords.latitude
-      this.gps_longitude = resp.coords.longitude
-      this.getRegion(resp.coords.longitude,resp.coords.latitude)
-     }).catch((error) => {
-       console.log('Error getting location', error);
-     });
+  constructor(
+    private geolocation: Geolocation,
+    private httpClient: HttpClient,
+    private formBuilder: FormBuilder,
+  ) {
+    this.getMyLocation()
+    this.getIssLocation()
+    this.itemForm = this.formBuilder.group({
+      name: '',
+      address: '',
+    })
+  }
 
-    const userAction = async () => {
-      const response = await fetch('http://api.open-notify.org/iss-now.json');
-      const myJson = await response.json(); //extract JSON from the http response
-      this.iss_latitude = myJson.iss_position.latitude
-      this.iss_longitude = myJson.iss_position.longitude
-      console.log(myJson)
-    }
+  autoCalculation() {
+    this.swiper = false
+    this.getMyLocation()
+  }
 
-    userAction()
+  getMyLocation() {
+    this.geolocation
+      .getCurrentPosition()
+      .then((resp) => {
+        this.gps_latitude = resp.coords.latitude
+        this.gps_longitude = resp.coords.longitude
+        this.getRegion(resp.coords.longitude, resp.coords.latitude)
+      })
+      .catch((error) => {
+        console.log('Error getting location', error)
+      })
+  }
 
+  async getIssLocation() {
+    await this.httpClient
+      .get('http://api.open-notify.org/iss-now.json')
+      .subscribe((response: any) => {
+        this.iss_latitude = response.iss_position.latitude
+        this.iss_longitude = response.iss_position.longitude
+      })
   }
 
   getRegion(long, late) {
+    // Create a request variable and assign a new XMLHttpRequest object to it.
+    var request = new XMLHttpRequest()
 
-     // Create a request variable and assign a new XMLHttpRequest object to it.
-     var request = new XMLHttpRequest()
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open(
+      'GET',
+      'http://open.mapquestapi.com/geocoding/v1/reverse?key=ZohDzfogWWQncGWVtf3ZNQddHR6wZpZv&location=' +
+        late +
+        ',' +
+        long,
+      true,
+    )
 
-     // Open a new connection, using the GET request on the URL endpoint
-     request.open('GET', 'http://open.mapquestapi.com/geocoding/v1/reverse?key=ZohDzfogWWQncGWVtf3ZNQddHR6wZpZv&location='+late+','+long, true)
- 
-     request.onload = () => {
-       // Begin accessing JSON data here
-       var data = JSON.parse(request.response)
- 
-       if (request.status >= 200 && request.status < 400) {
-         this.current_place = data.results[0]['locations'][0]['adminArea5']
-         this.current_zip = data.results[0]['locations'][0]['postalCode']
-       } else {
-         console.log('error')
-       }
-     }
- 
-     // Send request
-     request.send()
+    request.onload = () => {
+      // Begin accessing JSON data here
+      var data = JSON.parse(request.response)
+
+      if (request.status >= 200 && request.status < 400) {
+        this.current_place = data.results[0]['locations'][0]['adminArea5']
+        this.current_zip = data.results[0]['locations'][0]['postalCode']
+      } else {
+        console.log('error')
+      }
+    }
+
+    // Send request
+    request.send()
   }
-
-  ngOnInit(): void {}
 }
--- src/app/pages/calculation/calculation.module.ts
+++ src/app/pages/calculation/calculation.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { Geolocation } from '@ionic-native/geolocation/ngx'
 import { IonicModule } from '@ionic/angular'
 import { CalculationRoutingModule } from './calculation-routing.module'
 import { CalculationComponent } from './calculation.component'
-import { Geolocation } from '@ionic-native/geolocation/ngx'
 
 @NgModule({
   declarations: [CalculationComponent],
-  imports: [CommonModule, CalculationRoutingModule, IonicModule],
-  providers: [ Geolocation ],
+  imports: [CommonModule, CalculationRoutingModule, IonicModule, FormsModule, ReactiveFormsModule],
+  providers: [Geolocation],
 })
 export class CalculationModule {}
