79f7bbac1a1fece1888c59f32986168983884fbc
Luca Derivaz <derivaz.luca@gmail.com>
2020-12-12 14:52:23 +0100

    enhacne - page/calculation

--- src/app/pages/calculation/calculation.component.ts
+++ src/app/pages/calculation/calculation.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http'
 import { Component } from '@angular/core'
 import { FormBuilder } from '@angular/forms'
 import { Geolocation } from '@ionic-native/geolocation/ngx'
+import { IssInformation, UserInformation } from './calculation'
 
 @Component({
   selector: 'calculation',
@@ -9,41 +10,57 @@ import { Geolocation } from '@ionic-native/geolocation/ngx'
   styleUrls: ['./calculation.component.scss'],
 })
 export class CalculationComponent {
-  iss_latitude
-  iss_longitude
-  gps_latitude
-  gps_longitude
-  current_place
-  current_zip
+  issInfos: IssInformation = {
+    latitude: null,
+    longitude: null,
+  }
+  userInfos: UserInformation = {
+    latitude: null,
+    longitude: null,
+    zip: null,
+    place: null,
+  }
 
   swiper = false
   itemForm
+  calculationType = 'location_input'
 
   constructor(
     private geolocation: Geolocation,
     private httpClient: HttpClient,
     private formBuilder: FormBuilder,
   ) {
-    this.getMyLocation()
     this.getIssLocation()
     this.itemForm = this.formBuilder.group({
-      name: '',
-      address: '',
+      lat: '',
+      lng: '',
+      zip: '',
+      location: '',
     })
+  }
+
+  ionViewDidEnter(): void {
+    this.drawIss()
+    this.getMyLocation()
   }
 
   autoCalculation() {
     this.swiper = false
     this.getMyLocation()
+    this.drawMyPosition(this.userInfos)
   }
 
-  getMyLocation() {
-    this.geolocation
+  changeCalculationType($event) {
+    this.calculationType = $event
+  }
+
+  async getMyLocation() {
+    await this.geolocation
       .getCurrentPosition()
       .then((resp) => {
-        this.gps_latitude = resp.coords.latitude
-        this.gps_longitude = resp.coords.longitude
-        this.getRegion(resp.coords.longitude, resp.coords.latitude)
+        this.userInfos.latitude = resp.coords.latitude
+        this.userInfos.longitude = resp.coords.longitude
+        this.getLocation(resp.coords.longitude, resp.coords.latitude)
       })
       .catch((error) => {
         console.log('Error getting location', error)
@@ -54,38 +71,105 @@ export class CalculationComponent {
     await this.httpClient
       .get('http://api.open-notify.org/iss-now.json')
       .subscribe((response: any) => {
-        this.iss_latitude = response.iss_position.latitude
-        this.iss_longitude = response.iss_position.longitude
+        this.issInfos.latitude = response.iss_position.latitude
+        this.issInfos.longitude = response.iss_position.longitude
       })
   }
 
-  getRegion(long, late) {
-    // Create a request variable and assign a new XMLHttpRequest object to it.
-    var request = new XMLHttpRequest()
+  async getLocation(long, late) {
+    await this.httpClient
+      .get(
+        'http://open.mapquestapi.com/geocoding/v1/reverse?key=ZohDzfogWWQncGWVtf3ZNQddHR6wZpZv&location=' +
+          late +
+          ',' +
+          long,
+      )
+      .subscribe(
+        (response: any) => {
+          this.userInfos.place = response.results[0]['locations'][0]['adminArea5']
+          this.userInfos.zip = response.results[0]['locations'][0]['postalCode']
+        },
+        (error) => {
+          console.log(error)
+        },
+      )
+  }
 
-    // Open a new connection, using the GET request on the URL endpoint
-    request.open(
-      'GET',
-      'http://open.mapquestapi.com/geocoding/v1/reverse?key=ZohDzfogWWQncGWVtf3ZNQddHR6wZpZv&location=' +
-        late +
-        ',' +
-        long,
-      true,
-    )
+  async getCoordinates(location, zip) {
+    await this.httpClient
+      .get(
+        'http://open.mapquestapi.com/geocoding/v1/address?key=ZohDzfogWWQncGWVtf3ZNQddHR6wZpZv&location=' +
+          location +
+          ' ,' +
+          zip,
+      )
+      .subscribe(
+        (response: any) => {
+          console.log(response)
+          return response.results[0]['locations'][0]['adminArea5'].latLng
+        },
+        (error) => {
+          console.log(error)
+        },
+      )
+  }
 
-    request.onload = () => {
-      // Begin accessing JSON data here
-      var data = JSON.parse(request.response)
+  submit() {
+    if (this.calculationType == 'location_input') {
+      let coordinates = this.getCoordinates(
+        this.itemForm.get('location').value,
+        this.itemForm.get('zip').value,
+      )
+      this.drawMyPosition(coordinates)
+    }
+    if (this.calculationType == 'coordinates_input') {
+      // @TODO implement calculation
+    }
+  }
 
-      if (request.status >= 200 && request.status < 400) {
-        this.current_place = data.results[0]['locations'][0]['adminArea5']
-        this.current_zip = data.results[0]['locations'][0]['postalCode']
-      } else {
-        console.log('error')
-      }
+  drawIss() {
+    let e = document.getElementById('tracker').getBoundingClientRect()
+    let width = e.width
+    let height = e.height
+
+    let c = <HTMLCanvasElement>document.getElementById('tracker')
+    let ctx = c.getContext('2d')
+  }
+
+  drawMyPosition(coordinates) {
+    console.log('circle')
+    let e = document.getElementById('tracker').getBoundingClientRect()
+    let width = e.width
+    let height = e.height
+
+    let c = <HTMLCanvasElement>document.getElementById('tracker')
+    let ctx = c.getContext('2d')
+    ctx.beginPath()
+    ctx.arc(2, 1, 1, 0, 2 * Math.PI, true)
+    ctx.fillStyle = 'red'
+    ctx.fill()
+  }
+
+  calculate() {
+    // Setup
+    const a = 51.6219
+    const c = 360 / 383.568
+
+    var lat1 = -51.6219
+    var long1 = -92.2547
+
+    var lat2 = -51.6199
+    var long2 = 24.7959
+
+    // Calc
+    var y_a = lat1 / a // y:a
+
+    var b = (Math.asin(y_a) * 180) / Math.PI / c - long1
+    if (lat1 > lat2) {
+      var x2 = (90 / c - long1) * 2 + long1
+      b = x2 - (Math.asin(y_a) * 180) / Math.PI / c
     }
 
-    // Send request
-    request.send()
+    b = (Math.PI / 180) * b
   }
 }
---
+++ src/app/pages/calculation/calculation.d.ts
@@ -0,0 +1,11 @@
+export interface IssInformation {
+  latitude: number
+  longitude: number
+}
+
+export interface UserInformation {
+  latitude: number
+  longitude: number
+  place: string
+  zip: number
+}
